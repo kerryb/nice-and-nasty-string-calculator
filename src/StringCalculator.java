174f57120628860efede9cc298b2e09222a7531f
Kerry Buckley <kerryjbuckley@gmail.com>
2022-10-08 18:07:15 +0100

    Return a single number

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,5 +1,9 @@
 public class StringCalculator {
     public int add(String input) {
-        return 0;
+        if (input.isEmpty()) {
+            return 0;
+        } else {
+            return Integer.parseInt(input);
+        }
     }
 }

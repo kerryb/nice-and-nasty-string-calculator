c5b4bb4a7e15d424bdbd5064d944a83afa24444c
Kerry Buckley <kerryjbuckley@gmail.com>
2022-10-08 18:13:26 +0100

    Return sum of two comma-separated numbers

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,9 +1,13 @@
+import java.util.Arrays;
+
 public class StringCalculator {
     public int add(String input) {
         if (input.isEmpty()) {
             return 0;
         } else {
-            return Integer.parseInt(input);
+            return Arrays.stream(input.split(","))
+                    .mapToInt((s) -> Integer.parseInt(s))
+                    .sum();
         }
     }
 }

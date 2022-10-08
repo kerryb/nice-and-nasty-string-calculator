0e5bf28e22ff23bccb5ce0ecfc5f337ff874ced9
Kerry Buckley <kerryjbuckley@gmail.com>
2022-10-08 18:25:57 +0100

    Add test to confirm mixed commas and newlines work

--- test/StringCalculatorTest.java
+++ test/StringCalculatorTest.java
@@ -24,4 +24,9 @@ public class StringCalculatorTest {
     public void addsNewlineSeparatedNumbers() {
         assertEquals(42, calc.add("20\n2\n6\n14"));
     }
+
+    @Test
+    public void addsMixedCommaAndNewlineSeparatedNumbers() {
+        assertEquals(42, calc.add("20,2\n6,14"));
+    }
 }

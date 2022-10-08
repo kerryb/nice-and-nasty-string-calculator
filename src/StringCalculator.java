f3cbb94550f8e9e20a187b44bb11545579c3a39d
Kerry Buckley <kerryjbuckley@gmail.com>
2022-10-08 14:47:18 +0100

    Allow custom delimiter

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,11 +1,22 @@
 public class StringCalculator {
     public int add(String input) {
+        String remainingInput;
+        int sum = 0;
+        int commaIndex, newlineIndex, index;
+
         if (input.isEmpty()) {
             return 0;
+        } else if (input.startsWith("//")) {
+            String delimiter = input.substring(2, 3);
+            remainingInput = input.substring(4);
+            while (remainingInput.contains(delimiter)) {
+                index = remainingInput.indexOf(delimiter);
+                sum += Integer.parseInt(remainingInput.substring(0, index));
+                remainingInput = remainingInput.substring(index + 1);
+            }
+            return sum + Integer.parseInt(remainingInput);
         } else if (input.contains(",") || input.contains("\n")) {
-            String remainingInput = input;
-            int sum = 0;
-            int commaIndex, newlineIndex, index;
+            remainingInput = input;
             while (remainingInput.contains(",") || remainingInput.contains("\n")) {
                 commaIndex = remainingInput.indexOf(",");
                 newlineIndex = remainingInput.indexOf("\n");
--- test/StringCalculatorTest.java
+++ test/StringCalculatorTest.java
@@ -29,4 +29,9 @@ public class StringCalculatorTest {
     public void addsMixedCommaAndNewlineSeparatedNumbers() {
         assertEquals(42, calc.add("20,2\n6,14"));
     }
+
+    @Test
+    public void addsNumbersWithCustomDelimiter() {
+        assertEquals(42, calc.add("//:\n20:2:6:14"));
+    }
 }

c7cba75ffadc90a13875d65356d2b3a64dba97fe
Kerry Buckley <kerryjbuckley@gmail.com>
2022-10-08 18:00:59 +0100

    Return zero for empty string

---
+++ test/StringCalculatorTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class StringCalculatorTest {
+    private final StringCalculator calc = new StringCalculator();
+
+    @Test
+    void returnsZeroForEmptyString() {
+        assertEquals(0, calc.add(""));
+    }
+}

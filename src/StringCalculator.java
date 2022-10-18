ff01884f0e2d5fe8e6a397e4b04686b38ff9a01b
Kerry Buckley <kerryjbuckley@gmail.com>
2022-10-18 14:11:45 +0100

    Extract sumWithCustomDelimiter method

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -6,11 +6,15 @@ public class StringCalculator {
         if (input.isEmpty()) {
             return 0;
         } else if (input.startsWith("//")) {
-            var delimiter = input.substring(2, 3);
-            return sumWithDelimiter(input.substring(4), delimiter);
+            return sumWithCustomDelimiter(input);
         } else {
             return sumWithDelimiter(input, "[,\\n]");
         }
+    }
+
+    private int sumWithCustomDelimiter(String input) {
+        var delimiter = input.substring(2, 3);
+        return sumWithDelimiter(input.substring(4), delimiter);
     }
 
     private int sumWithDelimiter(String input, String regex) {
